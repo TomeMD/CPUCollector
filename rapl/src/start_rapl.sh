c640ed25da3674192295b81902babadf207e67b6
TomeMD <tome.maseda@udc.es>
2023-02-12 17:52:42 +0100

    Added script to execute rapl_plot

---
+++ rapl/src/start_rapl.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+chmod 666 /dev/cpu/*/msr
+echo 0 > /proc/sys/kernel/perf_event_paranoid
+
+cp /var/lib/rapl/rapl_plot/rapl_plot /tmp/rapl_plot
+
+setcap cap_sys_rawio=ep /tmp/rapl_plot
+
+echo "Starting RAPL monitor"
+./tmp/rapl_plot ${RAPL_OUTPUT_PREFIX} ${RAPL_SECONDS_INTERVAL}

49f43e761f6f9d25a84c71a2c80d5bc398f7a736
TomeMD <tome.maseda@udc.es>
2023-03-01 12:17:13 +0100

    Modified RAPL image build (Docker and Apptainer)

--- rapl/src/start_rapl.sh
+++ rapl/src/start_rapl.sh
@@ -2,12 +2,5 @@
 
 export LD_LIBRARY_PATH=/usr/local/lib
 
-chmod 666 /dev/cpu/*/msr
-echo 0 > /proc/sys/kernel/perf_event_paranoid
-
-cp /var/lib/rapl/rapl_plot/rapl_plot /tmp/rapl_plot
-
-setcap cap_sys_rawio=ep /tmp/rapl_plot
-
 echo "Starting RAPL monitor"
 /tmp/rapl_plot ${RAPL_OUTPUT_PREFIX} ${RAPL_SECONDS_INTERVAL}

8e33999298c32b9dbaa58ddab6c910d41f601294
TomeMD <tome.maseda@udc.es>
2023-09-19 11:02:12 +0200

    Simplified CPUFreq script

--- cpufreq/get-freq-percore.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for cpu_path in /sys/devices/system/cpu/cpu*/cpufreq/scaling_cur_freq; do
-    affected_path="${cpu_path/scaling_cur_freq/affected_cpus}"   
-    echo $(cat "$affected_path") $(cat "$cpu_path")
-    echo ""
-done
--- cpufreq/get-freq.sh
+++ cpufreq/get-freq.sh
@@ -4,30 +4,21 @@ export HOME=`dirname $0`
 
 while true; do
     # Get active cores
-    OUTPUT=$(mpstat -P ALL 1 1 | grep "Average" | tail -n +3 | awk '$NF < 90 {print $2}')
-    CORES=""
-    for CORE in $OUTPUT; do
-        if [[ -z "$CORES" ]]; then
-            CORES="${CORE}"
-        else
-            CORES="${CORES},${CORE}"
-        fi
-    done
+    ACTIVE_CORES=$(mpstat -P ALL 1 1 | grep "Average" | tail -n +3 | awk '$NF < 50 {print $2}')
+    readarray -t ACTIVE_CORES_ARRAY <<< "$ACTIVE_CORES"
 
-    CORE_PATTERN=$(echo $CORES | sed 's/,/\|/g')
-    OUTPUT=$(${HOME}/get-freq-percore.sh | egrep "^($CORE_PATTERN) ")
-
+    # Get average frequency from active cores
     SUM=0
     COUNT=0
-    # Get average frequency from active cores
-    while read -r line; do
-        FREQ=$(echo $line | awk '{print $2}')
+    for CORE in "${ACTIVE_CORES_ARRAY[@]}"; do 
+        FREQ=$(cat "/sys/devices/system/cpu/cpu${CORE}/cpufreq/scaling_cur_freq")
         SUM=$((SUM + FREQ))
         COUNT=$((COUNT + 1))
-    done <<< "$OUTPUT"
-
+    done
     AVERAGE=$((SUM / COUNT))
     AVERAGE_MHZ=$((AVERAGE / 1000))
+
+    # Send data to InfluxDB
     TIMESTAMP=$(date +%s%N)
     DATA="cpu_frequency average=${AVERAGE_MHZ},count=${COUNT} ${TIMESTAMP}"
     curl -i -XPOST "http://montoxo.des.udc.es:8086/api/v2/write?org=MyOrg&bucket=glances" --header "Authorization: Token MyToken" --data-binary "${DATA}" > /dev/null 2>&1

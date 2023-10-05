cdb6170115901b808a1a427ac982898ca14fd76a
TomeMD <tome.maseda@udc.es>
2023-10-05 10:44:59 +0200

    Added support to monitor per core frequency

--- cpufreq/get-freq.sh
+++ cpufreq/get-freq.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
+TOTAL_CORES=$(lscpu | grep -m 1 "CPU(s):" | awk '{print $2}')
+declare -A CORE_DICT
+
 while true; do
     # Get active cores
-    ACTIVE_CORES=$(mpstat -P ALL 1 1 | awk '/Average|Media/ && $2 !~ /^(CPU|all)$/ && $NF < 50 {print $2}')
-
-    if [ -z "$ACTIVE_CORES" ]; then
-        continue
-    fi
-
-    # Get average frequency from active cores
-    SUM=0
-    COUNT=0
-    for CORE in ${ACTIVE_CORES[@]}; do
-        FREQ=$(<"/sys/devices/system/cpu/cpu${CORE}/cpufreq/scaling_cur_freq")
-        SUM=$((SUM + FREQ))
-        COUNT=$((COUNT + 1))
+    ACTIVE_CORES=$(mpstat -P ALL 1 1 | awk '/Average|Media/ && $2 !~ /^(CPU|all)$/ && ($NF + 0) < 50 {print $2}')
+    for CORE in $ACTIVE_CORES; do
+      CORE_DICT[$CORE]=1
     done
 
-    AVERAGE=$((SUM / COUNT / 1000))
+    SUM=0
+    ACTIVE_SUM=0
+    ACTIVE_COUNT=0
+    for ((i=0; i < TOTAL_CORES; i++)); do
+      FREQ=$(<"/sys/devices/system/cpu/cpu${i}/cpufreq/scaling_cur_freq")
+      SUM=$((SUM + FREQ))
+      if [[ ${CORE_DICT[$i]} -eq 1 ]]; then
+        ACTIVE_SUM=$((ACTIVE_SUM + FREQ))
+        ACTIVE_COUNT=$((ACTIVE_COUNT + 1))
+	      CORE_DICT[$i]=0
+      fi
+    done
+
+    AVERAGE=$((SUM / TOTAL_CORES/ 1000))
+    if [[ ACTIVE_COUNT -ne 0 ]]; then
+      ACTIVE_AVERAGE=$((ACTIVE_SUM / ACTIVE_COUNT / 1000))
+    else
+      ACTIVE_AVERAGE=0
+    fi
 
     # Send data to InfluxDB
     TIMESTAMP=$(date +%s%N)
-    DATA="cpu_frequency average=${AVERAGE},count=${COUNT} ${TIMESTAMP}"
-    curl -s -XPOST "http://montoxo.des.udc.es:8086/api/v2/write?org=MyOrg&bucket=glances" --header "Authorization: Token MyToken" --data-binary "${DATA}"
+    DATA="cpu_frequency mpstat_average=${ACTIVE_AVERAGE},mpstat_sum=${ACTIVE_SUM},mpstat_count=${ACTIVE_COUNT},total_average=${AVERAGE} ${TIMESTAMP}"
+    #curl -s -XPOST "http://montoxo.des.udc.es:8086/api/v2/write?org=MyOrg&bucket=glances" --header "Authorization: Token MyToken" --data-binary "${DATA}"
 done
